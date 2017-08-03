ffefe009588a211c079c3d86171b4b4bab635e2c
Fabricio <Fabricio@Anthony>
2017-08-03 11:35:55 -0600

    final

--- src/taller/pkg1/main.java
+++ src/taller/pkg1/main.java
@@ -16,6 +16,9 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        menu me = new menu();
+        me.setVisible(true);
+        
     }
     
 }
